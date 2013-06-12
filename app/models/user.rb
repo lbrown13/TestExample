1dc158397f08a589cee6c25a007d08fa4a6aa700
Lawrence Brown <lawrence.brown@gmail.com>
2013-06-12 16:11:18 -0500

    Pushing inmidst of merging TRs CSS Files

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role_ids
 
   after_create :add_user_to_mailchimp
   before_destroy :remove_user_from_mailchimp
